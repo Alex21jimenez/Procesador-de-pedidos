5a68d6315969099f64a403ea5a83b99fc2ec19ac
Alex21jimenez <alexjimenezmejorada@gmail.com>
2025-02-11 11:24:09 -0600

    Optimized app.py and fixed locale issues

--- app.py
+++ app.py
@@ -1,41 +1,40 @@
 import pandas as pd
-import tkinter as tk
-from tkinter import filedialog
 from flask import Flask, render_template, request
-import locale
-
-# Configurar idioma en español
-
 
 app = Flask(__name__)
 
 data_global = []
 meses_global = []
 
-def procesar_archivo(file_path):
-    df = pd.read_excel(file_path)
-    columnas_interes = ["LIN03", "REF02", "FST01", "FST04", "FST09"]
-    df_limpio = df[columnas_interes].copy()
-    df_limpio.loc[:, "FST04"] = pd.to_datetime(df_limpio["FST04"], format="%Y%m%d")
-    df_limpio.loc[:, "Semana"] = df_limpio["FST04"].dt.to_period("W").astype(str)
-    meses_traduccion = {
+# Diccionario para traducir los meses a español
+MESES_TRADUCCION = {
     'January': 'Enero', 'February': 'Febrero', 'March': 'Marzo', 'April': 'Abril',
     'May': 'Mayo', 'June': 'Junio', 'July': 'Julio', 'August': 'Agosto',
     'September': 'Septiembre', 'October': 'Octubre', 'November': 'Noviembre', 'December': 'Diciembre'
 }
-df_limpio.loc[:, "Mes"] = df_limpio["FST04"].dt.strftime('%B').map(meses_traduccion)
 
-    df_pivot = df_limpio.pivot_table(index=["LIN03", "REF02"], 
-                                     columns="Semana", 
-                                     values="FST01", 
-                                     aggfunc="sum", 
+def procesar_archivo(file_path):
+    df = pd.read_excel(file_path, dtype=str)
+    columnas_interes = ["LIN03", "REF02", "FST01", "FST04", "FST09"]
+    df_limpio = df[columnas_interes].copy()
+    df_limpio["FST04"] = pd.to_datetime(df_limpio["FST04"], format="%Y%m%d")
+    df_limpio["Semana"] = df_limpio["FST04"].dt.to_period("W").astype(str)
+    df_limpio["Mes"] = df_limpio["FST04"].dt.strftime('%B').map(MESES_TRADUCCION)
+    
+    df_pivot = df_limpio.pivot_table(index=["LIN03", "REF02"],
+                                     columns="Semana",
+                                     values="FST01",
+                                     aggfunc="sum",
                                      fill_value=0)
     df_pivot.reset_index(inplace=True)
     semanas_numeros = [str(pd.to_datetime(semana.split("/")[0]).weekofyear) for semana in df_pivot.columns[2:]]
-    meses = [pd.to_datetime(semana.split("/")[0]).strftime('%B').capitalize() for semana in df_pivot.columns[2:]]
+    meses = [pd.to_datetime(semana.split("/")[0]).strftime('%B').map(MESES_TRADUCCION) for semana in df_pivot.columns[2:]]
+    
     df_pivot.columns = ["Número de Parte", "Nombre de la Parte"] + semanas_numeros
+    
     meses_global.clear()
     meses_global.extend(meses)
+    
     return df_pivot.to_dict(orient='records')
 
 @app.route('/')
